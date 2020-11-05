321ccdec2608b1161635247f7584a7de249a2a96
German Albarracin <germanga77@gmail.com>
2020-11-05 15:38:16 -0300

    js detecta dispositivo

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+   device = "mobile";
+ }else{
+   device = "computer"
+ }
+
+ console.log(device)
 
  function enviar () {
 
@@ -7,15 +14,24 @@
     var pegarnumero = document.getElementById("pegarnumero").value.toString() ;
     
     var tosend = pais + area + numero
+    var a = document.getElementById('link');
 
-    document.write 
+       
     console.log(tosend);
-
+   
+    var linkmobile = "https://wa.me/" + tosend;
     var link = "https://web.whatsapp.com/send?phone=" + tosend + "&text&app_absent=0";
-    console.log(link)
 
-    var a = document.getElementById('link'); //or grab it by tagname etc
-    a.href = link
+    console.log(link)
+    console.log(linkmobile)
+
+     //or grab it by tagname etc
+   switch(device){
+      case "mobile": a.href = linkmobile;
+      case "computer": a.href = link;
+   }
+   
+    
 
     // document.body.appendChild(element);
     }
